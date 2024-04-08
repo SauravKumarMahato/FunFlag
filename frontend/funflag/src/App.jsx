6a2aa4d03ccb8f9933708fb47b06ff514ee25de8
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2024-04-08 21:14:13 +0545

    changed all other files inside src folder

---
+++ frontend/funflag/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Footer } from './components/Footer/Footer';
+import { NavbarCustom } from './components/Navbar/Navbar';
+import AppRoute from './components/routes/AppRoute'
+
+
+function App() {
+
+  return (
+    <>
+      <NavbarCustom />
+      <AppRoute />
+      <Footer />
+
+    </>
+  )
+}
+
+export default App;
